f0099652f67e99d37c8056abe1b41e1b80a41703
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-02-18 01:50:22 +0430

    repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (name, repeate) {
+  if (repeate < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < repeate; i++) {
+    string += name;
+  }
+  return string;
 };
 
 // Do not edit below this line
